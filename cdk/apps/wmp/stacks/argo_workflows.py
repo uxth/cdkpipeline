916f91975cf6719512b5779be729f09c45125638
ning.xu <ning.xu@tusimple.ai>
2021-07-13 19:50:04 -0700

    secret arn to secret name

--- cdk/apps/wmp/stacks/argo_workflows.py
+++ cdk/apps/wmp/stacks/argo_workflows.py
@@ -10,16 +10,15 @@ from utils.configBuilder import Config
 class ArgoWorkflowsStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, eks_stack: EksStack, config: Config, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        secret_arn = secretmanager.Secret.from_secret_name(
+        secret_arn = secretmanager.Secret.from_secret_name_v2(
             self, 'rds_secret',
             secret_name=config.getValue('rds.admin_secret_name')
         ).secret_arn
         manifest = yamlParser.readYaml(path=config.getValue('wmp.argo-workflow.secrets'))
         manifest['stringData']['password'] = core.SecretValue.secrets_manager(
-            secret_id=secret_arn,
+            secret_id=config.getValue('rds.admin_secret_name'),
             # secret_id='arn:aws:secretsmanager:us-west-2:711208530951:secret:map_rds_admin-1prLs8',
             json_field='password').to_string()
-
         manifests = yamlParser.readManifest(paths=config.getValue('wmp.argo-workflow.manifests'))
         manifests.append(manifest)
         # install argo workflows
@@ -32,7 +31,7 @@ class ArgoWorkflowsStack(core.Stack):
 
         yaml = yamlParser.readYaml(path=config.getValue('wmp.argo-workflow.valuesPath'))
         yaml['controller']['persistence']['postgresql']['host'] = core.SecretValue.secrets_manager(
-            secret_id=secret_arn,
+            secret_id=config.getValue('rds.admin_secret_name'),
             json_field='host').to_string()
         helm = eks.HelmChart(
             self, id='wmp-argo-workflows', cluster=eks_stack.cluster, chart='argo-workflows',
