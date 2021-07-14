6897d1fb52d49db2ec88f30d36ac4ef6038efbbb
ning.xu <ning.xu@tusimple.ai>
2021-07-13 18:42:48 -0700

    change the string random generator

--- utils/randGenerator.py
+++ utils/randGenerator.py
@@ -3,10 +3,13 @@ import random
 
 
 def stringGenerator(length: int):
-    letters = string.ascii_letters + string.digits + string.punctuation
-    res = random.choice(string.ascii_letters)
-    res = res.join(random.choice(string.digits))
-    res = res.join(random.choice(string.punctuation))
-    res = res.join(random.choice(letters) for i in range(length - 3))
-    # print(res)
+    res = ''
+    while len(res) < length:
+        if len(res) < length:
+            res += random.choice(string.ascii_letters)
+        if len(res) < length:
+            res += random.choice(string.digits)
+        if len(res) < length:
+            res += random.choice(string.punctuation)
+    print(res)
     return res
