747f1303353fb4f824b08c8002622435ee15af56
ning.xu <ning.xu@tusimple.ai>
2021-07-13 20:58:06 -0700

    try partial arn

--- cdk/apps/wmp/stacks/argo_workflows.py
+++ cdk/apps/wmp/stacks/argo_workflows.py
@@ -1,3 +1,5 @@
+import json
+
 from aws_cdk import aws_eks as eks
 from aws_cdk import core
 from cdk.common.stacks.eks import EksStack
@@ -10,13 +12,19 @@ from utils.configBuilder import Config
 class ArgoWorkflowsStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, eks_stack: EksStack, config: Config, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        secret = secretmanager.Secret.from_secret_name_v2(
-            self, 'rds_secret',
-            secret_name=config.getValue('rds.admin_secret_name')
-        )
+        secret = secretmanager.Secret.from_secret_partial_arn(
+            self, 'partial_arn',
+            self.format_arn(
+                service="secretmanager",
+                resource="secret",
+                resource_name=config.getValue('rds.admin_secret_name'),
+                account=config.getValue('common.AWSAccountID'),
+                region=config.getValue('common.AWSProfileRegion'),
+                sep=':'
+            ))
         manifest = yamlParser.readYaml(path=config.getValue('wmp.argo-workflow.secrets'))
-        json = secret.secret_value.to_json()
-        manifest['stringData']['password'] = json['password']
+
+        manifest['stringData']['password'] = secret.secret_value_from_json('password').to_string()
         # manifest['stringData']['password'] = core.SecretValue.secrets_manager(
         #     secret_id=config.getValue('wmp.argo-workflow.secret_arn'),
         #     json_field='password').to_string()
@@ -31,7 +39,7 @@ class ArgoWorkflowsStack(core.Stack):
         )
 
         yaml = yamlParser.readYaml(path=config.getValue('wmp.argo-workflow.valuesPath'))
-        yaml['controller']['persistence']['postgresql']['host'] = json['host']
+        yaml['controller']['persistence']['postgresql']['host'] = secret.secret_value_from_json('host').to_string()
         # yaml['controller']['persistence']['postgresql']['host'] = core.SecretValue.secrets_manager(
         #     secret_id=config.getValue('wmp.argo-workflow.secret_arn'),
         #     json_field='host').to_string()
